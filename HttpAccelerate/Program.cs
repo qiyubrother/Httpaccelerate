61a99945a64f236d3af4f5e6e0c4a3968259e64f
qiyubrother <13240137763@163.com>
2018-11-24 00:18:01 +0800

    添加项目文件。

---
+++ HttpAccelerate/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpAccelerate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var frm = new FrmStartup();
+            frm.ShowDialog();
+            while (true)
+            {
+                var fileName = "url.txt";
+                string[] urls = null;
+                try
+                {
+                    urls = System.IO.File.ReadAllLines(fileName);
+
+                    Parallel.ForEach(urls, (url) =>
+                    {
+                        if (!string.IsNullOrEmpty(url))
+                        {
+                            try
+                            {
+                                var request = (HttpWebRequest)WebRequest.Create(url);
+                                request.Credentials = CredentialCache.DefaultCredentials;
+                                request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)";
+                                var response = request.GetResponse();
+                                new StreamReader(response.GetResponseStream()).ReadToEnd();
+                                response.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                //Console.WriteLine(ex.Message);
+                            }
+                        }
+                    });
+                    //Console.WriteLine("[{0}]--OK.", DateTime.Now);
+                    Thread.Sleep(100);
+                }
+                catch (Exception ex2)
+                {
+                    //Console.WriteLine(ex2.Message);
+                }
+            }
+        }
+    }
+}
